92c193e8772200c54080db57d49371ca8fff0bd1
djmason9 <djmason9@0257db02-a610-11de-ac05-951101a8bf3e>
2010-01-03 00:10:29 +0000

    added new measurment types

--- trunk/PocketBartender/src/com/drinkmixer/utils/Constants.java
+++ trunk/PocketBartender/src/com/drinkmixer/utils/Constants.java
@@ -1,8 +1,12 @@
 package com.drinkmixer.utils;
 
 public class Constants {
-	public static String VERSION = "1.1.0";
+	public static String VERSION = "1.1.1";
 	public static String COMPANY_NAME = "mypocket-technologies";
 	
 	public static long selectedCat=-1;
+	
+	/**
+	 * 1.1.1 - added new measurement types
+	 */
 }
